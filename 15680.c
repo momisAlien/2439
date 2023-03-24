353e9ea4a6624886ccd533d198a19c46e82df35d
MomisAlien <127112536+momisAlien@users.noreply.github.com>
2023-03-24 13:58:45 +0900

    Add files via upload

---
+++ 15680.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int N;
+	scanf_s("%d", &N);
+	if (N == 0) {
+		printf("YONSEI");
+	}
+	if (N == 1) {
+		printf("Leading the Way to the Future");
+	}
+	
+	return 0;
+}
